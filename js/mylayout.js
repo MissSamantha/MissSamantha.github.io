6d3a9e8b59b21d07adfb7155f32a306449815d71
MissSamantha <18811158912@163.com>
2019-05-08 17:37:33 +0800

    Site updated: 2019-05-08 17:37:31

---
+++ js/mylayout.js
@@ -0,0 +1,6 @@
+
+window.onload=function (){
+    let divHeight = document.getElementById("main").clientHeight;
+    document.getElementsByClassName("left-div")[0].style.height = divHeight + 'px';
+}
+/*console.log(document.getElementsByClassName("main"));*/
